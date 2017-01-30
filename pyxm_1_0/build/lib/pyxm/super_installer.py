01f83be7026e89a4cf7d29162854516249043c36
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2017-01-29 20:34:01 -0700

    rebuilt scripts

--- pyxm_1_0/build/lib/pyxm/super_installer.py
+++
@@ -1,9 +0,0 @@
-import os
-
-api_key = raw_input('Please enter your api key here: \n>')
-
-os.system('pip install -r requirements.txt')
-os.system('touch ./bin/api_key.py')
-os.system('echo "__API_KEY__ = \'' + api_key + '\'" > "./bin/api_key.py"')
-os.system('python setup.py install')
-
